0da43a6e910623fcab6b0afe9dee99bdccf969b7
Sebastian Gonzalez <sebastian.gonzalez@kyndryl.com>
2023-03-29 16:37:34 -0300

    Adding PlatformSeba

---
+++ New folder/main.py
@@ -0,0 +1,19 @@
+# https://coderslegacy.com/python/pygame-platformer-game-development/
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+vec = pygame.math.Vector2
+
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+
+FramePerSec = pygame.time.Clock()
+
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Plataformas Seba")
+
