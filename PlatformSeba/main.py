b1ca68532c534287b574f2393591b0262eb93293
Sebastian Gonzalez <sebastian.gonzalez@kyndryl.com>
2023-03-29 16:59:44 -0300

    Added Player and Platform

--- PlatformSeba/main.py
+++ PlatformSeba/main.py
@@ -14,3 +14,23 @@ FramePerSec = pygame.time.Clock()
 
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Plataformas Seba")
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128,255,40))
+        self.rect = self.surf.get_rect(center = (10, 420))
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, 20))
+        self.surf.fill((255, 0, 0))
+        self.rect = self.surf.get_rect(center=(WIDTH / 2, HEIGHT - 10))
+
+
+PT1 = Platform()
+P1 = Player()
